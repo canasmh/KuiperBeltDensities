99032f9e4c7b8b2e27978168ce5f302b087648ba
canasmh <canasmh@yahoo.com>
2022-09-16 19:08:47 -0400

    Create a reader method, start working on the method that will be used to get the physical properties of the planetesimals formed from streaming instability

--- reader.py
+++ reader.py
@@ -1,10 +1,26 @@
-import csv
+from pandas import read_csv as csv
 import numpy as np
 
 
 class StreamingInstabilityData:
 
-    def __init__(self, rho_ice, rho_sil, file_path="./data/si-data"):
+    def __init__(self, rho_ice, rho_sil, initial_porosity=0.5, file_path="./data/si-data.csv"):
         self.rho_ice = rho_ice
         self.rho_sil = rho_sil
         self.file_path = file_path
+        self.initial_porosity = initial_porosity
+        self.__get_planetesimal_properties()
+
+    def __read_data(self):
+        data = csv(self.file_path)
+        
+        return data.n_ice, data.n_sil
+
+    def __get_planetesimal_properties(self):
+        n_ice, n_sil = self.__read_data()
+        self.porosity = np.repeat(self.initial_porosity, len(n_ice))
+
+
+if __name__ == "__main__":
+    kbos = StreamingInstabilityData(rho_ice=1, rho_sil=3.5)
+    
