e903fe83ada53b6932f28e99036e2da97ad566b9
canasmh <canasmh@yahoo.com>
2022-09-18 04:26:04 -0400

    Fix bug where volume and surface densities were returning negative numbers

--- pebbles.py
+++ pebbles.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from constants import AU_TO_CM
+
 
 class Pebbles:
     # TODO: Need to formulate the volume and column density distribution
@@ -25,7 +27,7 @@ class Pebbles:
         self.rho_ice = rho_ice
         self.model = model
         self.n_pebbles = n_pebbles
-        self.bimodal_split = 0.1  # Bimodal distribution splits at 1 mm
+        self.bimodal_split = 0.01  # Bimodal distribution splits at 1 mm
         self.__get_pebble_radius()
         if model == "constant":
             self.q = 0
@@ -68,9 +70,12 @@ class Pebbles:
                 rhops_test = rhops[i:].max()
 
                 if rhops_test > self.rho_sil:
-                    raise ValueError("Could not get the correct power law. Please ensure you  are using less than 4 significant figures.")
+                    raise ValueError("Could not get the correct power law. Please ensure you are using less than 4 significant figures.")
 
             rhops[:i] = np.repeat(self.rho_sil, i)
+
+        if q >= 0.5:
+            raise ValueError("Negative densities encounteres (q >= 0.5. This is resulting from needing a large exponent to get from rho_sil to rho_ice")
         
         self.density = rhops
         self.ice_fraction = abs(1 - ((self.density - self.density[-1]) / (self.density[0] - self.density[-1])))
@@ -89,15 +94,24 @@ class Pebbles:
         return self.mass * W * np.gradient(self.radius)
 
     def volume_density_distribution(self, rho_d, alpha):
-        
-        f = 3 * rho_d / (8 * np.pi * self.density * np.sqrt(self.radius.max())) * np.sqrt(1 + self.St / alpha) * self.radius ** (-3.5)
+        p = 0.5 + self.q
+        f = 3 * (1 - p) * rho_d / (4 * np.pi * self.rho_ice * np.sqrt(self.a_ice)) * np.sqrt(1 + self.St / alpha) * self.radius ** (-3.5)
 
         return self.mass * f * np.gradient(self.radius)
 
 
 if __name__ == "__main__":
     import time
-    n_par = 10
+    from gas_properties import gas_temp, column_density, scale_height
+    R = 20 * AU_TO_CM
+
+    T = gas_temp(R)
+    sigma_g = column_density(R)
+    H = scale_height(T, R)
+    rho_g = sigma_g / (np.sqrt(2 * np.pi) * H)
+    Z = 0.04
+    rho_d = Z * rho_g
+    n_par = 20
     models = ["decreasing", "bimodal", "constant"]
     sil_densities = [2.0, 2.25, 2.5, 2.75, 3.0, 3.25, 3.5, 3.75, 4.0, 3.7896, 3.956]
     ice_densities = [0.5, 1.0]
@@ -111,11 +125,16 @@ if __name__ == "__main__":
             for ice_density in ice_densities:
                 try:
                     pebbles = Pebbles(n_pebbles=n_par, a_min=a_min, a_max=a_max, rho_sil=sil_density, model=model, rho_ice=ice_density)
-                except ValueError:
+                    pebbles.stokes_number(H, rho_g)
+                    f_da = pebbles.volume_density_distribution(rho_d=rho_d, alpha=1e-4)
+                    W_da = pebbles.column_density_distribution(Z, sigma_g)
+                except ValueError as err:
                     print(f"Could not converge for density:\nrho_ice = {ice_density}\nrho_sil = {sil_density}")
+                    print(err)
+
                 time.sleep(1)
-                print(f"{' radius (cm) ':=^30}{' mass (g) ':=^30}{' density (g/cm3) ':=^30}{' ice fraction (%) ':=^30}")
+                print(f"{' radius (cm) ':=^20}{' mass (g) ':=^20}{' density (g/cm3) ':=^20}{' ice fraction (%) ':=^20}{' W(a)da (g/cm2) ':=^20}{' f(a)da (g/cm3) ':=^20}")
                 for i in range(pebbles.n_pebbles):
-                    print(f"{pebbles.radius[i]:^30.3e}{pebbles.mass[i]:^30.3e}{pebbles.density[i]:^30.3f}{pebbles.ice_fraction[i] * 100:^30.3f}")
+                    print(f"{pebbles.radius[i]:^20.3e}{pebbles.mass[i]:^20.3e}{pebbles.density[i]:^20.5f}{pebbles.ice_fraction[i] * 100:^20.5f}{W_da[i]:^20.5f}{f_da[i]:^20.3e}")
                 print("")
 
