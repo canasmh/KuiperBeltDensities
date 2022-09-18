902fbfde9dc36cfe0a7ea34609868566792c3aff
canasmh <canasmh@yahoo.com>
2022-09-18 19:22:06 -0400

    Establish the run parameters for the simulation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from constants import AU_TO_CM, M_PLUTO
+from constants import AU_TO_CM, M_PLUTO, YRS_TO_SEC
 from gas_properties import sound_speed, scale_height, kep_frequency, gas_temp, column_density
 from pebbles import Pebbles
 from reader import StreamingInstabilityData
@@ -26,9 +26,18 @@ kbos.add_masses(n_bins=50, m_per_bin=3, min_dens=min(kbos.density), max_dens=min
 pebbles = Pebbles(n_pebbles=100, a_min=1e-4, a_max=1.0, rho_sil=3.5, model='bimodal', rho_ice=1.0, scale_height=H, gas_density=rho_g)
 f_da = pebbles.volume_density_distribution(rho_d, alpha=1e-4)
 W_da = pebbles.column_density_distribution(Z=Z, gas_column_density=sigma_g)
+delta_v = 3000  # sub-keplerian velocity
 
-plt.plot(pebbles.radius, f_da)
-plt.xscale('log')
-plt.yscale('log')
-plt.show()
+
+# Run parameters
+IVAR = 12  # The last printed 'var' file from our simulation. Var prints every half orbit
+t_orb = 2 * np.pi / Omega
+tol = 0.005  # The maximum mass accretion rate per dt will be 0.5% of the mass of corresponding KBO
+dt_init = 10 * t_orb  # Set the initial time step to be 10 orbits
+t_min = IVAR * np.pi / (2 * np.pi) * t_orb  # Starting time is 6 orbits
+t_max = 4.5e6 * YRS_TO_SEC  # Simulation will run for 4.5 Million years
+t = t_min  # Set time to t_min
+
+while t < t_max:
+    t += dt_init
     
