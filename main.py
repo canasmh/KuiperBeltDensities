0fe0ff5ee54fd8422a96a5b2e31e4e54357e7bb4
canasmh <canasmh@yahoo.com>
2022-09-18 19:53:02 -0400

    Write functionality for getting the time step

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from constants import AU_TO_CM, M_PLUTO, YRS_TO_SEC
+from accretion_integrator import rk4
+from constants import AU_TO_CM, M_PLUTO, YRS_TO_SEC, G
 from gas_properties import sound_speed, scale_height, kep_frequency, gas_temp, column_density
 from pebbles import Pebbles
 from reader import StreamingInstabilityData
@@ -21,13 +22,15 @@ rho_d = Z * rho_g
 # Streaming Instability results
 kbos = StreamingInstabilityData(rho_ice=1, rho_sil=3.0, unit_mass=rho_g * (H ** 3))
 kbos.add_masses(n_bins=50, m_per_bin=3, min_dens=min(kbos.density), max_dens=min(kbos.density) + 0.1, min_mass=1e-3 * M_PLUTO, max_mass=1e-2 * M_PLUTO)
+n_mass = kbos.n_mass
 
 # Pebble Distribution
-pebbles = Pebbles(n_pebbles=100, a_min=1e-4, a_max=1.0, rho_sil=3.5, model='bimodal', rho_ice=1.0, scale_height=H, gas_density=rho_g)
+n_pebbles = 100
+pebbles = Pebbles(n_pebbles=n_pebbles, a_min=1e-4, a_max=1.0, rho_sil=3.5, model='bimodal', rho_ice=1.0, scale_height=H, gas_density=rho_g)
 f_da = pebbles.volume_density_distribution(rho_d, alpha=1e-4)
 W_da = pebbles.column_density_distribution(Z=Z, gas_column_density=sigma_g)
 delta_v = 3000  # sub-keplerian velocity
-
+transition_mass = np.sqrt(1 / 3) * delta_v ** 3 / G / Omega * (1 / 8) / pebbles.St # If mass > than this value, its in the Hill regime
 
 # Run parameters
 IVAR = 12  # The last printed 'var' file from our simulation. Var prints every half orbit
@@ -39,5 +42,40 @@ t_max = 4.5e6 * YRS_TO_SEC  # Simulation will run for 4.5 Million years
 t = t_min  # Set time to t_min
 
 while t < t_max:
+    dt = dt_init
+    rho_g = rho_g_init * np.exp(-t / t_max)
+    pebbles.stokes_number(H, rho_g)
+    transition_mass = np.sqrt(1 / 3) * delta_v ** 3 / G / Omega * (1 / 8) / pebbles.St
+    f_da = pebbles.volume_density_distribution(rho_d, alpha=1e-4)
+
+    M_BONDI = np.zeros([n_pebbles, n_mass])
+    M_FOCUS = np.zeros([n_pebbles, n_mass])
+    M_ADDED = np.zeros([n_pebbles, n_mass])
+
+    # Get the time step
+    mass_exceeded = True
+
+    while mass_exceeded:
+        for m in range(n_mass):
+            for p in range(n_pebbles):
+                if kbos.mass[m] < transition_mass[p]:
+                    M_FOCUS[p, m] = rk4(dt, acc_type='focus', m=kbos.mass[m], rho=kbos.density[m], rho_d=f_da[p], Omega=Omega, St=pebbles.St[p], deltav=delta_v, H_g=H)
+                    M_BONDI[p, m] = rk4(dt, acc_type='bondi', m=kbos.mass[m], r=R, rho_d=f_da[p], Omega=Omega, St=pebbles.St[p], deltav=delta_v, H_g=H)
+                    M_ADDED[p, m] = max(M_FOCUS[p, m], M_BONDI[p, m])
+
+                else:
+                    M_ADDED[p, m] = rk4(dt, acc_type='hill', m=kbos.mass[m], r=R, rho_d=f_da[p], Omega=Omega, St=pebbles.St[p], deltav=delta_v, H_g=H)
+            
+            if np.sum(M_ADDED[:, m]) > tol * kbos.mass[m]:
+                dt *= 0.9
+                break
+
+            elif m == n_mass - 1:
+                mass_exceeded = False
+
+
+
+
+
     t += dt_init
     
