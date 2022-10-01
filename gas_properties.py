bde4a1db57c138df6a7b9d4abc03d4f6e93271a6
canasmh <canasmh@yahoo.com>
2022-10-01 16:22:36 -0400

    Give the full expression of the column density which includes the exponential factor

--- gas_properties.py
+++ gas_properties.py
@@ -22,7 +22,7 @@ def scale_height(T, R):
 
 def column_density(r, m_disk=0.04*M_SUN, r_c=50*AU_TO_CM):
 
-    return m_disk / (2 * np.pi * r_c ** 2) * (r / r_c) ** (-1)
+    return m_disk / (2 * np.pi * r_c ** 2) * (r / r_c) ** (-1) * np.exp(- r / r_c)
 
 
 def gas_temp(r):
@@ -36,10 +36,12 @@ def toomre_q(c_s, omega, sigma):
 
 
 if __name__ == "__main__":
-    R = [10, 20, 30, 40, 50]
+    R = [1, 10, 20, 30, 40, 50]
+    r_c = 50 * AU_TO_CM
 
     for r in R:
         r *= AU_TO_CM
+        
         T = gas_temp(r)
         print(f"R: {r / AU_TO_CM } AU")
         print(f"T: {T} K")
@@ -47,4 +49,6 @@ if __name__ == "__main__":
         print(f"Orbital Frequency: {kep_frequency(r): .3e} 1/s")
         print(f"Scale Height: {scale_height(T, r): .3e} cm")
         print(f"Column Density: {column_density(r): .3e} g/cm2")
+        print(f"Column Density (non-truncated): {column_density(r) / np.exp(- r / r_c): .3e} g/cm2")
+        print(f"Toomre Q (non-truncated vs. truncated): {toomre_q(sound_speed(T), kep_frequency(r), column_density(r) / np.exp(- r / r_c)): .2f} - {toomre_q(sound_speed(T), kep_frequency(r), column_density(r)): .2f}")
         print("")
