cbce54edb9d044a535293ebaed684703eb6d8ae4
canasmh <canasmh@yahoo.com>
2022-10-04 13:03:02 -0400

    Convert data from csv file to physical units (i.e., mass, density and ice_fraction)

--- C++/reader.cpp
+++ C++/reader.cpp
@@ -23,10 +23,19 @@ class StreamingInstabilityData
     double dy = 0.00078125;
     double dz = 0.00078125;
 
-    StreamingInstabilityData(string filepath) {
+    double mgas_code = total_density * dx * dy * dz;
+    double mpar_code = mgas_code * eps / npar;
+    double mpar;
+
+    StreamingInstabilityData(float rho_ice, float rho_sil, float unit_mass, float initial_porosity, string filepath) {
         int i = 1;
         int n_ice = 0;
         int n_sil = 0;
+        rho_ice = rho_ice;
+        rho_sil = rho_sil;
+        unit_mass = unit_mass;
+        initial_porosity = initial_porosity;
+        mpar = mpar_code * unit_mass;
 
         ifstream SIData;
         SIData.open(filepath);
@@ -51,7 +60,15 @@ class StreamingInstabilityData
                 n_sil = stoi(line);
                 i++;
                 i++;
+                double tmp_ice_frac = n_ice / ((n_ice + n_sil) * 1.0);
+                double tmp_mass = ((n_ice + n_sil) * 1.0) * mpar;
+                double tmp_density = initial_porosity * ((rho_ice * tmp_ice_frac) + (rho_sil * (1 - tmp_ice_frac)));
+                cout << "tmp_ice_frac= " << tmp_ice_frac << endl;
+                cout << "tmp_mass= " << tmp_mass << endl;
 
+                ice_fraction.push_back(tmp_ice_frac);
+                mass.push_back(tmp_mass);
+                density.push_back(tmp_density);
             }   
         }
         SIData.close();
@@ -60,21 +77,12 @@ class StreamingInstabilityData
 
 int main() {
 
-    StreamingInstabilityData kbos("../data/si-data.csv");
+    StreamingInstabilityData kbos(1.0, 3.0, 2.823973078884959e+28, 0.5, "../data/si-data.csv");
+    cout << "Mass     " << "Density     " << "Ice fraction    %   " << endl;
+    for (int i=0; i < kbos.mass.size(); i++) {
 
-
-    
-
-    // ifstream myFile;
-    // myFile.open("../data/si-data.csv");
-
-    // while (myFile.good()) {
-    //     string line;
-    //     getline(myFile, line, ',');
-    //     cout << line << endl;
-    // }
-
-    // myFile.close();
+        cout << kbos.mass[i] << " " << kbos.density[i] << " " << kbos.ice_fraction[i] * 100 << endl;
+    }
 
     return 0;
 };
